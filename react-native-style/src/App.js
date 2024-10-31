c7369206ce074a07adb763188df085c2af57212f
17sky35 <17sky35@naver.com>
2024-10-31 17:20:48 +0900

    10.31 todo

--- react-native-style/src/App.js
+++ react-native-style/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text,View,StyleSheet,ScrollView } from "react-native";
 import { textStyles, viewStyles } from "./styles";
 import { Header,Contens,Footer } from "./components/Layout";
@@ -8,22 +8,28 @@ import AlignItemsTest from "./components/alignItemsTest";
 import ShadowBox from "./components/ShadowBox";
 import {Styled} from "./components/StyledComponent";
 import Button from "./components/Button";
-import styled from "styled-components";
+import styled,{ ThemeProvider } from "styled-components";
 import SignUpScreen from "./SignUpScreen";
+import Input from "./components/Input";
+import { theme,lightTheme,darkTheme } from "./theme";
+import { Switch } from "react-native";
 
 const Container = styled.View`
     flex:1;
-    background-color:center;
+    background-color:${props => props.theme.background};
     align-items:center;
     justify-content:center;
 `
 
 export default function App(){
+    const [isDark,setIsDark] = useState(false);
+    const _toggleSwitch = () => {setIsDark(!isDark)}
+
     return(
         <ScrollView>
-            {/* <View style={viewStyles.container}>
-                인라인과 클래스 스타일 혼용사용 가능
-                뒤에 오는 스타일이 앞의 스타일 덮어쓴다.
+            <View style={viewStyles.container}>
+                {/* 인라인과 클래스 스타일 혼용사용 가능
+                뒤에 오는 스타일이 앞의 스타일 덮어쓴다. */}
                 <Text style={[textStyles.text,{color:"green"}]}>Inline-Styling - Text</Text>
                 <Text style={[textStyles.text,textStyles.error]}>Inline-Styling - Error</Text>
                 <Header />
@@ -35,10 +41,16 @@ export default function App(){
                 <ShadowBox />
                 <Styled />
             </View>
-            <Container>
-            <Button title="Hanbit"/>
-            <Button title="React Native"/>
-            </Container> */}
+            {/* ThemeProvider에 정의한 props는 하위 컴포넌트에서 받아서 사용할 수 있다. */}
+            <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+                <Container>
+                    <Switch value={isDark} onValueChange={_toggleSwitch} />
+                    <Button title="Hanbit"/>
+                    <Button title="React Native"/>
+                    <Input borderColor="#3498db" />
+                    <Input borderColor="#9b59b6" />
+                </Container>
+            </ThemeProvider>
             <SignUpScreen />
         </ScrollView>
     )
--- react-native-style/src/SignUpScreen.js
+++ react-native-style/src/SignUpScreen.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const View = styled.View`
-    alignItems:"center",
+    margin:20px;
 `
 const TitleText = styled.Text`
-    font-size:40px; 
+    textAlign:center;
+    font-size:50px;
+    marginBottom:10px;
 `
 const AText = styled.Text`
-    font-size:20px; 
+    margin:10px 0px;
+    font-size:30px; 
 `
 const TextInput = styled.TextInput`
-    font-size:20px; 
+    paddingLeft:10px;
+    border:1px solid;
+    height: 50px;
+    font-size:25px; 
+    border-radius:10px;
 `
 const Button = styled.Pressable`
-    background-color:blue;
+    alignItems:center
+    marginTop:40px;
+    background-color:red;
+    border-radius:10px;
 `
 
 const SignUpScreen = ()=>{
+    const [password,setPassword] = useState("");
+    const [passwords,setPasswords] = useState("");
     return(
         <View>
             <TitleText>회원가입</TitleText>
@@ -26,10 +38,12 @@ const SignUpScreen = ()=>{
             <AText>이메일</AText>
             <TextInput placeholder="이메일을 입력하세요" />
             <AText>비밀번호</AText>
-            <TextInput placeholder="비밀번호를 입력하세요" />
+            <TextInput onChangeText={setPassword} secureTextEntry={true} placeholder="비밀번호를 입력하세요" />
             <AText>비밀번호 확인</AText>
-            <TextInput placeholder="비밀번호를 입력하세요" />
-            {/* <Button>가입하기</Button> */}
+            <TextInput onChangeText={setPasswords}  secureTextEntry={true} placeholder="비밀번호를 입력하세요" />
+            <Button onPress={()=>(password===passwords? alert("가입완료"):alert("비번이다름"))} >
+                <AText>가입하기</AText>
+            </Button>
         </View>
     )
 }
