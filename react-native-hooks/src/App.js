46ad609d58825c8cfc27b0569a841de81faec996
17sky35 <17sky35@naver.com>
2024-11-05 15:29:33 +0900

    11.05 react-native

--- react-native-hooks/src/App.js
+++ react-native-hooks/src/App.js
@@ -5,6 +5,9 @@ import Form from "./components/Form";
 import Button from "./components/Button";
 import Timer from "./components/Timer";
 import Length from "./components/Length";
+import Dog from "./components/Dog";
+import MakeBread from "./components/makeBread";
+import { ScrollView } from "react-native";
 
 const Container = styled.View`
     flex:1;
@@ -16,16 +19,20 @@ const Container = styled.View`
 export default AppRegistry=()=>{
     const [isVisible,setIsVisible] = useState(true);
     return(
-        <Container>
-            {isVisible && <Form />}
-            {/* 버튼을 누르면 Form 컴포넌트를 보였다가 숨겼다가 만들기
-            버튼의 타이틀은 hide 또는 show로 바꾸기 */}
-            <Button 
-                title={isVisible?"hide":"show"} 
-                onPress={()=>setIsVisible(isVisible=>!isVisible)}
-            />
-            {/* <Timer /> */}
-            <Length />
-        </Container>
+        <ScrollView>
+            <Container>
+                {isVisible && <Form />}
+                {/* 버튼을 누르면 Form 컴포넌트를 보였다가 숨겼다가 만들기
+                버튼의 타이틀은 hide 또는 show로 바꾸기 */}
+                <Button 
+                    title={isVisible?"hide":"show"} 
+                    onPress={()=>setIsVisible(isVisible=>!isVisible)}
+                />
+                {/* <Timer /> */}
+                <Length />
+                <Dog />
+                <MakeBread />
+            </Container>
+        </ScrollView>
     )
 }
