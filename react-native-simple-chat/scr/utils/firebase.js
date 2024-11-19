7a00981cd6c625caf59ba42b3e2ece74b5b4b8b1
17sky35 <17sky35@naver.com>
2024-11-19 16:44:58 +0900

    11.19 react-native-simple-chat

--- react-native-simple-chat/scr/utils/firebase.js
+++ react-native-simple-chat/scr/utils/firebase.js
@@ -9,6 +9,7 @@ import { getStorage,uploadBytes,ref, getDownloadURL } from 'firebase/storage';
 import config from '../../firebase.json'
 import { collection, doc, getFirestore, setDoc } from 'firebase/firestore';
 
+
 //initializeApp()메서드
 //firebase는 전달된 설정을 기반으로 객체를 생성한다.
 //이후 app을 통해서 firebase의 인증, 데이터베이스, 스토리지등의 서비스를 이용할 것이다.
@@ -103,11 +104,15 @@ export const createChannel = async({title,description}) => {
     return id;
 }
 
-export const createMessage = async ({channelId,text}) => {
+export const createMessage = async ({channelId,message}) => {
     //특정 채널의 메시지 컬렉션 안에 새로운 메시지 문서의 레퍼런스를 생성
-    const docRef = doc(db,`channels/${channelId}/messages`,text);
+    const docRef = doc(db,`channels/${channelId}/messages`,message._id);
     //생성된 문서 레퍼런스에 메시지 데이터를 저장
     //기존 메시지 객체의 모든 속성을 복사하고
     //createdAt필드를 현재 시간으로 추가.
-    await setDoc(docRef,{...text, createdAt: Date.now() });
+    await setDoc(docRef,{
+        ...message, 
+        createdAt: Date.now() 
+    });//현재 시간을 타임스탬프로 추가
+    console.log(`Message created with ID : ${docRef.id}`);
 }
